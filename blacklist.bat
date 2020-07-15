f506d719cac2951d30f448983aa522e41f90b769
6100m <31662463+6100m@users.noreply.github.com>
2020-07-15 13:03:54 -0600

    Finalize blacklist compiler + Fix error

--- blacklist.bat
+++ blacklist.bat
@@ -1,4 +1,14 @@
+:header
+@echo off
+goto main0
+:main0
+set "var=%cd%"
+set Pathname="%var%\cdn"
+cd /d %Pathname%
+if errorlevel 1 echo "cd Unsuccessful, performing pushd operation" && pushd %Pathname% && goto main1
+:main1
 copy /b cdn/*.txt cdn/compiled.txt
-pause
-cls
-exit
+echo "Follow the prompts to exit"
+goto terminate
+:terminate
+FOR /L %%N IN () DO pause && cls && exit
