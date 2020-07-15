9c30ff707144f7b04332dde9875dd11f5c8fa4a9
6100m <31662463+6100m@users.noreply.github.com>
2020-07-15 13:30:36 -0600

    Create constants-twitch.js

---
+++ src/constants-twitch.js
@@ -0,0 +1,9 @@
+/* Modified from https://github.com/techno-tim/techno-boto-twitch/blob/master/src/constants.js
+# export const CHANNEL_NAME = '#TwitchPlaysPokemon';
+# export const OAUTH_TOKEN = 'oauth:yourToken';
+# export const BOT_USERNAME = '6100mBot';
+# export const BLOCKED_WORDS = [
+#  'cats',
+#  'dogs',
+# ]
+# We comment all of this out, since code-patch.sh will write all the code needed for this file into this file, so to speak.
