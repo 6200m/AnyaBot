0f018cac6fb886e0217fc63e471535c3b4b20fd2
6100m <31662463+6100m@users.noreply.github.com>
2020-07-15 13:15:24 -0600

    v2 of blacklist.txt

--- blacklist.bat
+++ blacklist.bat
@@ -3,14 +3,30 @@
 goto main0
 :main0
 set "var=%cd%"
-set Pathname="%var%\cdn"
-cd /d %Pathname%
-if errorlevel 1 echo "cd was unsuccessful, performing pushd operation" && pushd %Pathname% && goto main1
+cd /d %cd%
+if errorlevel 1 echo "cd was unsuccessful, performing pushd operation" && pushd %cd% && goto main1
 :main1
-set "var=%cd%"
-set Pathname="%var%\cdn"
-copy /b %var%\*.txt %var%\compiled.txt
-echo "Follow the prompts to exit"
+set Pathname="cdn"
+cd /d %Pathname%
+if errorlevel 1 echo "cd was unsuccessful, performing pushd operation" && pushd %Pathname% && goto main2
+:main2
+copy /b *.txt compiled.txt
+goto main3
+:main3
+@echo off
+setLocal EnableDelayedExpansion
+for /f "tokens=* delims= " %%a in (compiled.txt) do (
+set /a N+=1
+echo ^  '%%a^',>>output.txt
+)
+goto assemble
+:assemble
+del compiled.txt
+ren output.txt compiled.txt
+goto exit
+:exit
+echo Follow the prompts to exit
+pause
 goto terminate
 :terminate
 FOR /L %%N IN () DO pause && cls && exit
