e34d1cb651deab7a04d9c6c948d553b1b9788c03
6100m <31662463+6100m@users.noreply.github.com>
2020-07-15 14:20:49 -0600

    Fix slight typos.

--- code_patch.sh
+++ code_patch.sh
@@ -5,8 +5,8 @@ cat <<EOT >> constants-twitch.js
 EOT
 value=$(<compiled.txt)
 echo "$value" >> "constants-twitch.js"
-mkdir bin-debug\shell
-mv compiled.txt bin-debug\shell
+mkdir bin-debug\shell\
+mv compiled.txt bin-debug\shell\
 del src\constants-twitch.sh
 mv constants-twitch.sh src\
 exit
