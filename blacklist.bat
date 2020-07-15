d33d616091236c22502d5f74ccaaf79e4659fbef
6100m <31662463+6100m@users.noreply.github.com>
2020-07-15 13:05:41 -0600

    Enforce variable usage, and fix another error, and even fix some typos too.

--- blacklist.bat
+++ blacklist.bat
@@ -5,9 +5,11 @@ goto main0
 set "var=%cd%"
 set Pathname="%var%\cdn"
 cd /d %Pathname%
-if errorlevel 1 echo "cd Unsuccessful, performing pushd operation" && pushd %Pathname% && goto main1
+if errorlevel 1 echo "cd was unsuccessful, performing pushd operation" && pushd %Pathname% && goto main1
 :main1
-copy /b cdn/*.txt cdn/compiled.txt
+set "var=%cd%"
+set Pathname="%var%\cdn"
+copy /b %var%\*.txt %var%\compiled.txt
 echo "Follow the prompts to exit"
 goto terminate
 :terminate
