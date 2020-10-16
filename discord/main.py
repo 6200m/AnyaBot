4f04c0cf2ba6fe024688ffb967c0200f5ae3850f
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-10-15 21:12:24 -0400

    Main

--- discord/main.py
+++ discord/main.py
@@ -1,1 +1,11 @@
-# Will work on this later
+# Basic right now, but will add more on request from 6100m
+import discord
+import json
+config = json.parse('discord.config')
+bot = commands.Bot(command_prefix=commands.when_mentioned_or("!"),
+                   description='anya moderation bot')
+@bot.event
+async def on_message():
+    if message in config['blacklist']:
+        message.delete()
+bot.run(config['token'])
